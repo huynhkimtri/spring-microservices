e74224c07ba2a5dacfa9e705eee86a5d73b51641
Kim-Tri Huynh <huynhkimtri.fuhcm@gmail.com>
2022-11-11 01:32:16 +0700

    Initial version of FullStackJHipsterMonolithicApp generated by generator-jhipster@7.9.3

---
+++ full-stack-jhipster/src/test/java/com/fullstackjhipster/config/SqlTestContainer.java
@@ -0,0 +1,9 @@
+package com.fullstackjhipster.config;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+
+public interface SqlTestContainer extends InitializingBean, DisposableBean {
+    JdbcDatabaseContainer<?> getTestContainer();
+}
---
+++ full-stack-jhipster/src/test/java/com/fullstackjhipster/cucumber/CucumberIT.java
@@ -0,0 +1,8 @@
+package com.fullstackjhipster.cucumber;
+
+import com.fullstackjhipster.IntegrationTest;
+import io.cucumber.junit.platform.engine.Cucumber;
+
+@Cucumber
+@IntegrationTest
+class CucumberIT {}
