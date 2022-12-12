32f4c7a547c48a79c40393f722d00c927ff959f9
Kim-Tri Huynh <huynhkimtri.fuhcm@gmail.com>
2022-12-13 02:07:07 +0700

    Init project jwt-auth-spring-microservices

---
+++ jwt-auth-spring-microservices/src/main/java/com/springfw/jwtauthspringmicroservices/JwtAuthSpringMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.springfw.jwtauthspringmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthSpringMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthSpringMicroservicesApplication.class, args);
+	}
+
+}
---
+++ jwt-auth-spring-microservices/src/test/java/com/springfw/jwtauthspringmicroservices/JwtAuthSpringMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springfw.jwtauthspringmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthSpringMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
